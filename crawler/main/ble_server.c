1e48d26375ab83eeb4c1272b508cb34ad35a434a
Wator Vapor <watorvapor@gmail.com>
2017-02-10 01:04:09 +0900

    Update ble_server.c

--- crawler/main/ble_server.c
+++ crawler/main/ble_server.c
@@ -17,6 +17,7 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_log.h"
