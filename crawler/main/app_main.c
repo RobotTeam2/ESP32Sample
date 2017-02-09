154fe3501d2fb144d4d83ced75db6ea33f66904e
Wator Vapor <watorvapor@gmail.com>
2017-02-09 23:51:32 +0900

    Update app_main.c

--- crawler/main/app_main.c
+++ crawler/main/app_main.c
@@ -1,7 +1,9 @@
-void ble_server_app_main(void);
 void blink_app_main();
+void signal_generator_app_main();
+void ble_server_app_main(void);
 void app_main()
 {
   blink_app_main();
+  signal_generator_app_main();
   ble_server_app_main();
 }
