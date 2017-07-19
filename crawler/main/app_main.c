21a5d286b08ca31081aab8cc02fe8ccf474cbc78
Wator Vapor <watorvapor@gmail.com>
2017-07-19 21:09:42 +0900

    Update app_main.c

--- crawler/main/app_main.c
+++ crawler/main/app_main.c
@@ -1,9 +1,11 @@
 void blink_app_main();
 void signal_generator_app_main();
 void ble_server_app_main(void);
+void nfc_app_main();
 void app_main()
 {
   blink_app_main();
   signal_generator_app_main();
   ble_server_app_main();
+  nfc_app_main();
 }
