9194b1fd364746e92038d925868ab75b39c32ab8
Wator Vapor <watorvapor@gmail.com>
2017-08-03 13:25:53 +0900

    Create MFRC522.h

---
+++ crawler/main/MFRC522.h
@@ -0,0 +1,18 @@
+class MFRC522 {
+
+public:
+    MFRC522();
+    void begin(void);
+    bool available();
+    void wait();
+    void readCardSerial();
+    byte *getCardSerial();
+    bool getBlock(byte block, byte keyType, byte *key, byte *returnBlock);
+    bool writeBlock(byte block, byte keyType, byte *key, byte *data);
+    bool communicate(byte command, byte *sendData, byte sendDataLength, byte *returnData, byte *returnDataLength);
+    void write(byte value);
+    byte read();
+    
+private:
+    byte cardSerial[4];    
+};
